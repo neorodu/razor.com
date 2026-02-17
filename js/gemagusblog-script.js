aae5ad1b7824c2714714e601a90ac4711d8355c7
GEMAGUS <knur13533@gmail.com>
2026-02-18 01:22:07 +0700

    Implement particle generator in gemagusblog-script.js
    Added a particle generator that creates animated particles on the screen.

--- js/gemagusblog-script.js
+++ js/gemagusblog-script.js
@@ -16,3 +16,26 @@ $(function() {
         $(".tm-header").removeClass("show");
       });
 });
+
+// ================= GENERATOR =================
+function createParticle() {
+  const particle = document.createElement("div");
+  particle.classList.add("particle");
+
+  const size = Math.random() * 6 + 2;
+  particle.style.width = size + "px";
+  particle.style.height = size + "px";
+
+  particle.style.left = Math.random() * window.innerWidth + "px";
+  particle.style.bottom = "-10px";
+
+  particle.style.animationDuration = (Math.random() * 5 + 5) + "s";
+
+  document.body.appendChild(particle);
+
+  setTimeout(() => {
+    particle.remove();
+  }, 10000);
+}
+
+setInterval(createParticle, 200);
